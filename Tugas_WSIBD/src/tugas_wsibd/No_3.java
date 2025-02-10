89d6141a815b37d8ca7e67f9741659f960555ca0
Farayuanita <farayntaa@gmail.com>
2025-02-10 20:56:09 +0700

    Create No_3.java

---
+++ Tugas_WSIBD/src/tugas_wsibd/No_3.java
@@ -0,0 +1,25 @@
+package Tugas_WSIBD;
+
+public class No_3 {
+    
+    static class book {
+        int price;
+        int pages;
+        
+    public void set (int price, int pages) {
+        this.price = price;
+        this.pages = pages;
+    }    
+       
+    public void show (){
+        System.out.println("books information");
+        System.out.println("books price     : " +price);
+        System.out.println("number of pages : " +pages);
+    }
+}
+        public static void main(String[] args) {
+            book javabook = new book();
+            javabook.set(60000, 100);
+            javabook.show();
+        }
+    }
