641b639281e2bffea60427583957191177e0ff5f
Farayuanita <farayntaa@gmail.com>
2025-02-10 20:55:15 +0700

    Create No_2.java

---
+++ Tugas_WSIBD/src/tugas_wsibd/No_2.java
@@ -0,0 +1,13 @@
+package Tugas_WSIBD;
+
+public class No_2 {
+    public static void main(String[] args) {
+        String nama = "Indonesia";
+        char abjad = 'a';
+        int a = 6;
+        double b = 6.9;
+        
+        System.out.println (nama);
+        System.out.println (a);
+    }
+}
