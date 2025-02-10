53fcd4165127b920e3b34d1e157e92af2c3577b0
Farayuanita <farayntaa@gmail.com>
2025-02-10 20:54:01 +0700

    Create No_1.java

---
+++ Tugas_WSIBD/src/tugas_wsibd/No_1.java
@@ -0,0 +1,12 @@
+package Tugas_WSIBD;
+
+public class No_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+        System.out.println("World !");
+        
+        
+        System.out.print("Hello ");
+        System.out.print("World !");
+    }
+}
