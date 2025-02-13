6beb43a00725aafc4b560d7e77f7a8dbd34278e1
Farayuanita <farayntaa@gmail.com>
2025-02-13 15:01:30 +0700

    Create Minggu2_No_2.java

---
+++ Tugas_WSIBD/src/tugas_wsibd/Minggu2_No_2.java
@@ -0,0 +1,43 @@
+package Tugas_WSIBD;
+import java.util.Scanner;
+
+public class Minggu2_No_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("          CAFE CERIA");
+        System.out.println("        ANEKA MINUMAN");
+        System.out.println("-----------------------------------");
+        System.out.println("        SPECIAL MENU :");
+        System.out.println("       1. Soft drinks");
+        System.out.println("       2. Mix juice");
+        System.out.println("       3. Nescafe");
+        System.out.println("       4. Soda milk");
+        System.out.println("       5. Tea");
+        System.out.println("-----------------------------------");
+        
+        System.out.print("Masukkan nama pembeli : ");
+        String nama = scanner.nextLine();
+        
+        System.out.print("\nSilahkan masukkan pilihan anda : ");
+        int pilihan = scanner.nextInt();
+        
+        String minuman = "";
+        switch (pilihan) {
+            case 1: minuman = "Soft drinks"; 
+            break;
+            case 2: minuman = "Mix juice"; 
+            break;
+            case 3: minuman = "Nescafe"; 
+            break;
+            case 4: minuman = "Soda milk"; 
+            break;
+            case 5: minuman = "Tea"; 
+            break;
+            default: System.out.println("Pilihan tidak tersedia"); return;
+        }
+        System.out.println("Minuman yang anda pesan adalah " + minuman);
+        System.out.println("Pesanan akan segera kami antar");
+        System.out.println("Terima Kasih " + nama + " telah berkunjung di Cafe Ceria");
+    }
+}
