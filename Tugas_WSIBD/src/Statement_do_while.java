0f7e8f8b0afee43970e6fd48dbabf74874c9b917
Farayuanita <farayntaa@gmail.com>
2025-02-17 18:59:25 +0700

    Create Statement_do_while.java

---
+++ Tugas_WSIBD/src/Statement_do_while.java
@@ -0,0 +1,12 @@
+public class Statement_do_while {
+    
+    public static void main(String[] args) {
+        
+        int i = 0;
+        
+        do {
+            System.out.println("perulangan ke-" + i);
+            i++;
+        } while ( i <= 10);
+    }
+}
